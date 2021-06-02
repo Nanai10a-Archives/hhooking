7a1c4b17490cc54deee1d3c8d0996c9f12144120
Nanai10a <Nanai10a@outlook.jp>
2021-06-02 18:38:31 +0900

    🚧 jsonをdecodeする関数の定義.

--- utils.go
+++ utils.go
@@ -3,6 +3,8 @@ package hhooking
 import (
 	"crypto/ed25519"
 	"net/http"
+    "encoding/json"
+    "io"
 
 	external "github.com/bsdlp/discord-interactions-go/interactions"
 )
@@ -11,3 +13,14 @@ func SignatureVerify(r *http.Request, key ed25519.PublicKey) bool {
     verified := external.Verify(r, key)
     return verified
 }
+
+func decodeJSON(r io.Reader, v *interface {}) {
+    bytes, err := io.ReadAll(r)
+
+    if err != nil {
+        // TODO: err handling
+    }
+
+    json.Unmarshal(bytes, v)
+}
+
