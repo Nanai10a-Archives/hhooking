85571380d34740d1f93c0669e20cc11ea90e68f6
Nanai10a <Nanai10a@outlook.jp>
2021-06-02 23:03:51 +0900

    🚧 decode関数の廃止.

--- utils.go
+++ utils.go
@@ -3,24 +3,11 @@ package hhooking
 import (
 	"crypto/ed25519"
 	"net/http"
-    "encoding/json"
-    "io"
 
 	external "github.com/bsdlp/discord-interactions-go/interactions"
 )
 
 func SignatureVerify(r *http.Request, key ed25519.PublicKey) bool {
-    verified := external.Verify(r, key)
-    return verified
+	verified := external.Verify(r, key)
+	return verified
 }
-
-func decodeJSON(r io.Reader, v *interface {}) {
-    bytes, err := io.ReadAll(r)
-
-    if err != nil {
-        // TODO: err handling
-    }
-
-    json.Unmarshal(bytes, v)
-}
-
