68d1ab0813d66f1a91425306a5bd6fad009eb27d
Nanai10a <Nanai10a@outlook.jp>
2021-06-03 13:30:14 +0900

    🚧 Interactions関係のRESTのwrapperを書き始めた: Get Global Application Commands

---
+++ rest.go
@@ -0,0 +1,50 @@
+package hhooking
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+const (
+	BaseAPIUrl = "https://discord.com/api/v8/"
+)
+
+func GetApplicationCommands(id SnowFlake, token string) []ApplicationCommand {
+	url, err := url.Parse(BaseAPIUrl)
+	if err != nil {
+		// TODO: err handling
+	}
+
+	url.Path = path.Join(url.Path, "applications", fmt.Sprintf("%d", id), "commands")
+
+	req, err := http.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		// TODO: err handling
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bot %s", token))
+
+	client := new(http.Client)
+	res, err := client.Do(req)
+	if err != nil {
+		// TODO: err handling
+	}
+
+	var v []ApplicationCommand
+	bytes, err := io.ReadAll(res.Body)
+	if err != nil {
+		// TODO: err handling
+	}
+
+	err = jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal(bytes, &v)
+	if err != nil {
+		// TODO: err handling
+	}
+
+	return v
+}
