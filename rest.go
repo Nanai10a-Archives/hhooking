f94af1074a40258b2433eba83bdd6f830be94779
Nanai10a <Nanai10a@outlook.jp>
2021-06-03 20:44:52 +0900

    🚧 impl: Edit Global Application Command + FIXME comments

--- rest.go
+++ rest.go
@@ -50,6 +50,27 @@ func GetGlobalApplicationCommand(appId SnowFlake, token string, cmdId SnowFlake)
     return v
 }
 
+// FIXME: 内容は同じだけどtagだけ違うので微妙なところではある "...PostData"との統合を検討.
+type ApplicationCommandPatchData struct {
+    Name string `json:"name,omitempty"`
+    Description string `json:"description,omitempty"`
+    Options []ApplicationCommandOption `json:"options,omitempty"`
+    DefaultPermisson bool `json:"default_permisson,omitempty"`
+}
+
+func EditGlobalApplicationCommand(appId SnowFlake, token string, cmdId SnowFlake, data ApplicationCommandPatchData) ApplicationCommand {
+    reqPath := path.Join("applications", fmt.Sprintf("%d", appId), "commands", fmt.Sprintf("%d", cmdId))
+    var v ApplicationCommand
+    content, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(data)
+    if err != nil {
+        // TODO: err handling
+    }
+    sendRequest(reqPath, "PATCH", content, token, &v)
+
+    return v
+}
+
+// FIXME: error返しません?
 func sendRequest(targetPath string, method string, content []byte, token string, rep interface {}) {
     url, err := url.Parse(BaseAPIUrl)
     if err != nil {
