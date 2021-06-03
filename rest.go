08c18900dc57a40160a27be9965377578fef55f5
Nanai10a <Nanai10a@outlook.jp>
2021-06-03 18:23:40 +0900

    🚧 request送信用の関数の作成.

--- rest.go
+++ rest.go
@@ -1,6 +1,7 @@
 package hhooking
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
@@ -48,3 +49,37 @@ func GetGlobalApplicationCommands(id SnowFlake, token string) []ApplicationComma
 
 	return v
 }
+
+func sendRequest(targetPath string, method string, content []byte, token string, rep interface {}) {
+    url, err := url.Parse(BaseAPIUrl)
+    if err != nil {
+        // TODO: err handling
+    }
+
+    url.Path = path.Join(url.Path, targetPath)
+
+    buf := bytes.NewBuffer(content)
+    req, err := http.NewRequest(method, url.String(), buf)
+    if err != nil {
+        // TODO: err handling
+    }
+
+    req.Header.Add("Authorization", token)
+
+    client := new(http.Client)
+    res, err := client.Do(req)
+    if err != nil {
+        // TODO: err handling
+    }
+
+    resBytes, err := io.ReadAll(res.Body)
+    if err != nil {
+        // TODO: err handling
+    }
+
+    err = jsoniter.ConfigCompatibleWithStandardLibrary.Unmarshal(resBytes, rep)
+    if err != nil {
+        // TODO: err handling
+    }
+}
+
