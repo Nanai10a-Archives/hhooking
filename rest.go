726cf73ad699626d741d002a39ffdb40a0061a28
Nanai10a <Nanai10a@outlook.jp>
2021-06-04 15:28:47 +0900

    🎨 formatted.

--- rest.go
+++ rest.go
@@ -130,65 +130,65 @@ func BulkOverwriteApplicationCommands(appId SnowFlake, authToken string, guildId
 }
 
 func CreateInteractionResponse(appId SnowFlake, authToken string, iaId SnowFlake, iaToken string, res InteractionReponse) {
-    reqPath := path.Join("interactions", fmt.Sprintf("%d", iaId), fmt.Sprintf("%d", iaToken), "callback")
-    sendRequest(reqPath, "POST", nil, authToken, nil)
+	reqPath := path.Join("interactions", fmt.Sprintf("%d", iaId), fmt.Sprintf("%d", iaToken), "callback")
+	sendRequest(reqPath, "POST", nil, authToken, nil)
 }
 
-type Message struct {}
+type Message struct{}
 
 func GetOriginalInteractionResponse(appId SnowFlake, authToken string, iaToken string) Message {
-    reqPath := path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprint("%d", iaToken), "messages", "@original")
-    var v Message
-    sendRequest(reqPath, "GET", nil, authToken, &v)
+	reqPath := path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprint("%d", iaToken), "messages", "@original")
+	var v Message
+	sendRequest(reqPath, "GET", nil, authToken, &v)
 
-    return v
+	return v
 }
 
-type WebhookMessagePatchData struct {}
+type WebhookMessagePatchData struct{}
 
 // FIXME: msgIdを使用すると"Edit Followup Message"なので命名に問題あり
 func EditOriginalInteractionResponse(appId SnowFlake, authToken string, iaToken string, msgId *SnowFlake, data WebhookMessagePatchData) Message {
-    var reqPath string
-    switch msgId {
-    case nil:
-        reqPath = path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprintf("%d", iaToken), "messages", "@original")
-    default:
-        reqPath = path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprintf("%d", iaToken), "messages", fmt.Sprintf("%d", *msgId))
-    }
+	var reqPath string
+	switch msgId {
+	case nil:
+		reqPath = path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprintf("%d", iaToken), "messages", "@original")
+	default:
+		reqPath = path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprintf("%d", iaToken), "messages", fmt.Sprintf("%d", *msgId))
+	}
 
-    var v Message
-    content, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(data)
-    if err != nil {
-        // TODO: err handling
-    }
-    sendRequest(reqPath, "PATCH", content, authToken, &v)
+	var v Message
+	content, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(data)
+	if err != nil {
+		// TODO: err handling
+	}
+	sendRequest(reqPath, "PATCH", content, authToken, &v)
 
-    return v
+	return v
 }
 
 // FIXME: msgIdを使用すると"Delete Followup Message"なので命名に問題あり
 func DeleteOriginalInteractionResponse(appId SnowFlake, authToken string, iaToken string, msgId *SnowFlake) {
-    var reqPath string
-    switch msgId {
-    case nil:
-        reqPath = path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprintf("%d", iaToken), "messages", "@original")
-    default:
-        reqPath = path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprintf("%d", iaToken), "messages", fmt.Sprintf("%d", *msgId))
-    }
+	var reqPath string
+	switch msgId {
+	case nil:
+		reqPath = path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprintf("%d", iaToken), "messages", "@original")
+	default:
+		reqPath = path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprintf("%d", iaToken), "messages", fmt.Sprintf("%d", *msgId))
+	}
 
-    sendRequest(reqPath, "DELETE", nil, authToken, nil)
+	sendRequest(reqPath, "DELETE", nil, authToken, nil)
 }
 
-type WebhookMessagePostData struct {}
+type WebhookMessagePostData struct{}
 
 func CreateFollowupMessage(appId SnowFlake, authToken string, iaToken string, data WebhookMessagePostData) {
-    reqPath := path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprintf("%d", iaToken))
-    content, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(data)
-    if err != nil {
-        // TODO: err handling
-    }
+	reqPath := path.Join("webhooks", fmt.Sprintf("%d", appId), fmt.Sprintf("%d", iaToken))
+	content, err := jsoniter.ConfigCompatibleWithStandardLibrary.Marshal(data)
+	if err != nil {
+		// TODO: err handling
+	}
 
-    sendRequest(reqPath, "POST", content, authToken, nil)
+	sendRequest(reqPath, "POST", content, authToken, nil)
 }
 
 // FIXME: error返しません?
